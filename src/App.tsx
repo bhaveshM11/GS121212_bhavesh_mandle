359cb5f4faf65a73ff05a268c8a46b8283bb03c7
Bhavesh <bhavesh.mandle@toprankers.com>
2025-04-23 21:18:30 +0530

    Added GTM Tag

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,34 @@ import './App.css'
 import Header from "./components/Header";
 import Tabs from "./components/Tabs"
 
+declare global {
+  interface Window {
+    dataLayer: Record<string, any>[];
+  }
+}
+
 function App() {
 
+  const handleClick = (e:React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    console.log("Clicked")
+     // Simulate successful login
+     const userId = "test123";
+     const method = "test_method";
+ 
+     // Push login event to GTM dataLayer
+     window.dataLayer = window.dataLayer || [];
+     window.dataLayer.push({
+       event: "user_login",
+       userId: userId,
+       method: method,
+     });
+ 
+     console.log("Login event pushed to dataLayer");
+  }
   return (
     <>
       <Header/>
+      <button onClick={(e)=>handleClick(e)}> GTM Track Login Button</button>
       <Tabs/>
     </>
   )
